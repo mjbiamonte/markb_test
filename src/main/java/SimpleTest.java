08bb94807943b43029d999514f5f8b66be9bbd74
datadirectbuild <ULTeamMarcos@progress.com>
2024-09-05 15:33:44 -0400

    Added argument display to test

--- src/main/java/SimpleTest.java
+++ src/main/java/SimpleTest.java
@@ -3,6 +3,26 @@ public class SimpleTest {
 
 	public static void main(String[] args) {
 
+		SimpleTest	test = new SimpleTest();
+		test.run(args);
+	}
+	
+	private void run(String[] args) {
+		
+		if (args.length == 0) {
+			
+			System.out.println("No input arguments specified");
+		}
+		else {
+			
+			System.out.print("Input Arguments:");
+			for (String curArg : args) {
+				
+				System.out.print(" " + curArg);
+			}
+			System.out.println();
+		}
+		
 		System.out.println("This Simple Test Worked");
 	}
 
